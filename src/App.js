1465ddd907ac4c84d174cd9b188f48a4b1c03b5d
Adit Sharda <mailme@aditsharda.com>
2024-11-11 22:00:27 -0500

    Fix invitation jpeg url.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import invite from 'invite.jpeg';
+import invite from './invite.jpeg';
 
 function App() {
   const [isOpened, setIsOpened] = useState(false);
@@ -14,7 +14,7 @@ function App() {
       <div className={`envelope ${isOpened ? 'open' : ''}`} onClick={handleOpen}>
         <div className="flap"></div>
         <div className="content">
-          <img src={`${process.env.PUBLIC_URL}/invitation.jpg`} alt="Invitation" className={`invitation ${isOpened ? 'spin' : ''}`} />
+          <img src={`${process.env.PUBLIC_URL}/invite.jpeg`} alt="Invitation" className={`invitation ${isOpened ? 'spin' : ''}`} />
         </div>
       </div>
     </div>
